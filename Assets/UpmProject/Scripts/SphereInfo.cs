c299ba3e1b873d23867cb57adeeebdc82b6b9518
MikeWise2718 <mwise@microsoft.com>
2020-01-09 13:01:14 +0100

    first

---
+++ Assets/UpmProject/Scripts/SphereInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereInfo : MonoBehaviour
+{
+    public string version = "v1.0.0";
+    public int updatecount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Version:{version}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatecount++;
+    }
+}
